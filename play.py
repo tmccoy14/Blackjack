289364c99ca92bbc7454924519624d21c115898c
tmccoy14 <tuckermmccoy@gmail.com>
2019-09-10 20:37:03 -0400

    updated comments and card total tracker

--- play.py
+++ play.py
@@ -16,7 +16,7 @@ class Blackjack:
 
         self.playerCard1 = self.cardDeck
         self.playerCard2 = self.cardDeck
-        self.doubleCard = self.cardDeck
+        self.doubleDownCard = self.cardDeck
 
         self.playerTotalCardAmount = 0
         self.dealerTotalCardAmount = 0
@@ -27,18 +27,21 @@ class Blackjack:
 
     # Make a blackjack simulator to simulate one hand of blackjack
     def blackJackDealerFunction(self):
+        # allow player to pick bet amount
         self.bet = input("Please place your bet - $5, $25, $50, $100: ")
 
+        # randomly deal dealer two cards and show first one
         dealerFirstCard = random.choice(self.dealerCard1)
         dealerSecondCard = random.choice(self.dealerCard2)
-
         print("\nDealer: {}".format(dealerFirstCard))
 
+        # randomly deal player two cards and show them
         playerFirstCard = random.choice(self.playerCard1)
         playerSecondCard = random.choice(self.playerCard2)
-
         print("Player: {}, {}".format(playerFirstCard, playerSecondCard))
 
+        # if either of the players cards is a face card, convert it to a value of 10
+        # otherwise convert the value to int and assign it to the card
         if playerFirstCard == "J" or playerFirstCard == "Q" or playerFirstCard == "K" or playerFirstCard == "A":
             playerFirstCard = 10
         else:
@@ -49,31 +52,36 @@ class Blackjack:
         else:
             playerSecondCard = int(playerSecondCard)
 
+        # get the value of the two cards to keep track of the total number
         self.playerTotalCardAmount += playerFirstCard
         self.playerTotalCardAmount += playerSecondCard
         print(self.playerTotalCardAmount)
 
+        # while loop to allow the player to keep making decisions while his/her total amount is below 21
         while self.playerTotalCardAmount <= 21:
 
+            # decision input from the user from the following options
             decision = input("\nDouble, Hit, Split, Stand: ")
 
-            # if user decides to double down on bet
+            # if user decides to double down on bet he/she gets one more card only and their turn is over
             if decision == "Double":
-                doubleCard = random.choice(self.doubleCard)
-                if playerFirstCard == 'J' or 'Q' or 'K' or 'A':
-                    self.playerTotalCardAmount += 10
+                doubleDownCard = random.choice(self.doubleDownCard)
+                if doubleDownCard == "J" or doubleDownCard == "Q" or doubleDownCard == "K" or doubleDownCard == "A":
+                    doubleDownCard = 10
+                    self.playerTotalCardAmount += int(doubleDownCard)
                 else:
-                    self.playerTotalCardAmount += int(doubleCard)
+                    self.playerTotalCardAmount += int(doubleDownCard)
                 if self.playerTotalCardAmount == 21:
-                    print("Player: {}, {}, {} -- BLACKJACK!".format(playerFirstCard, playerSecondCard, doubleCard))
+                    print("Player: {}, {}, {} -- {} BLACKJACK!".format(playerFirstCard, playerSecondCard, doubleDownCard, self.playerTotalCardAmount))
                     self.bet = self.bet * 1.5
                     print("You win ${}!".format(self.bet))
                     break
                 elif self.playerTotalCardAmount > 21:
-                    print("Player: {}, {}, {} -- BUST :'(".format(playerFirstCard, playerSecondCard, doubleCard))
+                    print("Player: {}, {}, {} -- {} BUST".format(playerFirstCard, playerSecondCard, doubleDownCard, self.playerTotalCardAmount))
+                    print("You lose ${}.".format(self.bet))
                     break
                 else:
-                    print("Player: {}, {}, {} -- {}".format(playerFirstCard, playerSecondCard, doubleCard, self.playerTotalCardAmount))
+                    print("Player: {}, {}, {} -- {}".format(playerFirstCard, playerSecondCard, doubleDownCard, self.playerTotalCardAmount))
                     break
             elif decision == "Hit":
 
